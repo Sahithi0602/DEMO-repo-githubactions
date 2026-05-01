46c2a2dc3feda291d672dd6bc51a1f8a41e63070
Sahithi0602 <85388736+Sahithi0602@users.noreply.github.com>
2026-05-01 18:50:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "one" {
   count = 2
   ami           = "ami-0ed094fb1304fd857"
-  instance_type = "t3.micro"
+  instance_type = "t3.micro" 
   tags = {
     Name = "dev-server-gab"
   }
